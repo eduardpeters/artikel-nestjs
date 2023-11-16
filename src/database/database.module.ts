2746796b2a7fef03fb332cb5aec62abb8e208a3b
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-11-16 20:47:28 +0100

    Setup database module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,27 @@
 import { Module } from '@nestjs/common';
 import { DatabaseService } from './database.service';
+import { ConfigService } from '@nestjs/config';
+import { Pool } from 'pg';
+
+async function databasePoolFactory(configService: ConfigService) {
+  return new Pool({
+    user: configService.get('POSTGRES_USER'),
+    host: configService.get('POSTGRES_HOST'),
+    database: configService.get('POSTGRES_DB'),
+    password: configService.get('POSTGRES_PASSWORD'),
+    port: configService.get('POSTGRES_PORT'),
+  });
+}
 
 @Module({
-  providers: [DatabaseService],
+  providers: [
+    {
+      provide: 'DATABASE_POOL',
+      inject: [ConfigService],
+      useFactory: databasePoolFactory,
+    },
+    DatabaseService,
+  ],
+  exports: [DatabaseService],
 })
 export class DatabaseModule {}
