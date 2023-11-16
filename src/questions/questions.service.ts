178b465524f99b1d492bdb6836c884d6fca1de8f
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-11-16 20:59:12 +0100

    Cleanup questions service

--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,33 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { Articles } from 'src/common/types/Articles.enum';
 import { GetQuestionsFilterDto } from './dto/get-questions-filter.dto';
-import { Categories } from 'src/common/types/Categories.enum';
 import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class QuestionsService {
   constructor(private databaseService: DatabaseService) {}
 
-  questions = [
-    {
-      text: 'Auto',
-      article: Articles.DAS,
-      choices: [Articles.DER, Articles.DIE, Articles.DAS],
-      category: Categories.NOUNS,
-    },
-    {
-      text: 'Lampe',
-      article: Articles.DIE,
-      choices: [Articles.DER, Articles.DIE, Articles.DAS],
-      category: Categories.NOUNS,
-    },
-    {
-      text: 'Regen',
-      article: Articles.DER,
-      choices: [Articles.DER, Articles.DIE, Articles.DAS],
-      category: Categories.NOUNS,
-    },
-  ];
   async getQuestions(filterDto: GetQuestionsFilterDto) {
     const { category = 'ALL', amount = 10 } = filterDto;
     const queryText = 'SELECT * FROM questions';
