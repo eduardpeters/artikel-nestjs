020493aa936effb85f28838038a10a1fb45d2cdd
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-11-23 20:46:25 +0100

    Implement practice route getQuestions

--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
 import { GetQuestionsFilterDto } from './dto/get-questions-filter.dto';
+import { Categories } from 'src/common/types/Categories.enum';
 
 @Injectable()
 export class QuestionsService {
   constructor(private databaseService: DatabaseService) {}
 
   async getQuestions(filterDto: GetQuestionsFilterDto) {
-    const { category = 'ALL', amount = 10 } = filterDto;
-    const queryText = 'SELECT * FROM questions';
-    return await this.databaseService.executeQuery(queryText);
+    const { category = Categories.ALL, amount = 10 } = filterDto;
+    let whereClause = '';
+    const values = [amount];
+    if (category !== Categories.ALL) {
+      whereClause = ` WHERE a.category = $2`;
+      values.push(category === Categories.NOUNS ? 0 : 1);
+    }
+    const queryText = `SELECT q.text, a.name AS article, ARRAY(SELECT articles.name FROM articles) AS choices FROM questions q JOIN articles a ON q.article = a.id${whereClause} LIMIT $1;`;
+    return await this.databaseService.executeQuery(queryText, values);
   }
 }
