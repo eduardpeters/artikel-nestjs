416a862d620bf9089986637025937cf22766674e
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-12-21 10:03:47 -0600

    Connect controller and service for articles

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ArticlesService } from './articles.service';
 
 @Controller('articles')
-export class ArticlesController {}
+export class ArticlesController {
+  constructor(private articlesService: ArticlesService) {}
+
+  @Get()
+  getArticles() {
+    return this.articlesService.getArticles();
+  }
+}
--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ArticlesService {}
+export class ArticlesService {
+  getArticles() {
+    return 'hello world';
+  }
+}
